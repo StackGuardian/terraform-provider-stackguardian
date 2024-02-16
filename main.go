bf6bad705283fbb0d876394fb11dd08451b8b3ec
piroux-sg <pierre.roux@stackguardian.io>
2024-02-16 01:42:33 +0100

    Fix provider go module name and location

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
 
-	stackguardian_tf_provider "github.com/StackGuardian/terraform-provider-stackguardian/stackguardian-tf-provider"
+	provider "github.com/StackGuardian/terraform-provider-stackguardian/internal/provider"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return stackguardian_tf_provider.Provider()
+			return provider.Provider()
 		},
 	})
 }
