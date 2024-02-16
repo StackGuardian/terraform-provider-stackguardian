644437dfe1332510b37e3a3e6269592f873c808f
piroux-sg <pierre.roux@stackguardian.io>
2024-02-16 01:42:33 +0100

    Bump Terraform SDK to latest v2

--- internal/provider/workflow_outputs_object_test.go
+++ internal/provider/workflow_outputs_object_test.go
@@ -4,9 +4,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 /*
@@ -18,12 +17,12 @@ TODO:
 */
 
 // var testAccProviders map[string]*schema.Provider
-var testAccProviders map[string]terraform.ResourceProvider
+var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
 
 func init() {
-	testAccProvider = Provider().(*schema.Provider)
-	testAccProviders = map[string]terraform.ResourceProvider{
+	testAccProvider = Provider()
+	testAccProviders = map[string]*schema.Provider{
 		"stackguardian": testAccProvider,
 	}
 }
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	provider "github.com/StackGuardian/terraform-provider-stackguardian/internal/provider"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return provider.Provider()
-		},
-	})
+	plugin.Serve(
+		&plugin.ServeOpts{
+			ProviderFunc: provider.Provider,
+			// TODO: fill in ProviderAddr
+		})
 }
