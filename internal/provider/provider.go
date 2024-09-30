6efd98204fd27cbd5f9a4239f7d63696c169fa12
Taher Kathanawala <taher.kathanawala@stackguardian.io>
2024-09-30 18:58:25 +0530

    Hard code the api_uri to restrict user to use the dash and not prod till production release

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -162,6 +162,10 @@ func (p *stackguardianProvider) Configure(ctx context.Context, req provider.Conf
 				"Either set the value statically in the configuration, or use the STACKGUARDIAN_API_URI environment variable.",
 		)
 	}
+
+	// TODO: Restrict usage of dash env for pre release. Uncomment before production release
+	api_uri = "https://testapi.qa.stackguardian.io"
+
 	client := sgclient.NewClient(
 		sgoption.WithApiKey("apikey "+api_key),
 		sgoption.WithBaseURL(api_uri),
