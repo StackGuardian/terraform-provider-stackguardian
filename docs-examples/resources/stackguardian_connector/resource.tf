75a75d7522e10d5d181254b1b843c275b63031db
AKSHAT TANDON <akshat.tandon@stackguardian.io>
2024-10-15 16:11:21 +0200

    fix the connector example

--- docs-examples/resources/stackguardian_connector/resource.tf
+++ docs-examples/resources/stackguardian_connector/resource.tf
@@ -1,14 +1,14 @@
 resource "stackguardian_connector" "aws-cloud-connector-example" {
-  resource_name = "aws-cloud-connector-example"
-  description   = "Example of terraform-provider-stackguardian for AWS Cloud Connector"
+  resource_name = "aws-rbac-connector"
+  description   = "AWS Cloud Connector"
 
   settings = {
-    kind   = "AWS_STATIC" # Type of connector. In this case, AWS_STATIC for static credentials.
+    kind   = "AWS_RBAC"
     
     config = [{
-      aws_access_key_id     = "YOUR_AWS_ACCESS_KEY_ID"       # Replace with your actual AWS Access Key ID
-      aws_secret_access_key = "YOUR_AWS_SECRET_ACCESS_KEY"   # Replace with your actual AWS Secret Access Key
-      aws_default_region    = "eu-central-1"                 # Specify the AWS region (e.g., eu-central-1)
+      roleArn     = "arn:aws:iam::209502960327:role/StackGuardian"
+      externalId = "wicked-hop:ElfygiFglfldTwnDFpAScQkvgvHTGV "
+      durationSeconds    = "3600"
     }]
   }
 }
