b549a5e67af076a6aceb91bd6419ee1e6bfa97d3
Taher Kathanawala <taher.kathanawala@stackguardian.io>
2024-10-21 15:28:36 +0530

    stack outputs datasource

---
+++ internal/datasources/stack_outputs/schema.go
@@ -0,0 +1,30 @@
+package stackoutputs
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Schema defines the schema for the data source.
+func (d *stackOutputsDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"stack": schema.StringAttribute{
+				Required: true,
+			},
+			"workflow_group": schema.StringAttribute{
+				Required: true,
+			},
+			"data": schema.MapAttribute{
+				ElementType: types.StringType,
+				Computed:    true,
+			},
+			"data_json": schema.StringAttribute{
+				Computed: true,
+			},
+		},
+	}
+}
